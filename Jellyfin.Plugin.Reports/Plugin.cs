b17fd052a266123147a60b4285594c05f9c9e80a
The Sundering <tadaspaura@gmail.com>
2025-12-30 23:06:41 -0500

    first commit

---
+++ Jellyfin.Plugin.Reports/Plugin.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using Jellyfin.Plugin.Reports.Configuration;
+using MediaBrowser.Common.Configuration;
+using MediaBrowser.Common.Plugins;
+using MediaBrowser.Model.Plugins;
+using MediaBrowser.Model.Serialization;
+
+namespace Jellyfin.Plugin.Reports
+{
+    public class Plugin : BasePlugin<PluginConfiguration>, IHasWebPages
+    {
+        public Plugin(IApplicationPaths appPaths, IXmlSerializer xmlSerializer)
+            : base(appPaths, xmlSerializer)
+        {
+        }
+
+        public override string Name => "Reports";
+
+        public override string Description => "Generate Reports";
+
+        public PluginConfiguration PluginConfiguration => Configuration;
+
+        public override Guid Id { get; } = new Guid("d4312cd9-5c90-4f38-82e8-51da566790e8");
+
+        public IEnumerable<PluginPageInfo> GetPages()
+        {
+            return new PluginPageInfo[]
+            {
+                new PluginPageInfo
+                {
+                    Name = "reports",
+                    EmbeddedResourcePath = GetType().Namespace + ".Web.reports.html",
+                    EnableInMainMenu = true
+                },
+                new PluginPageInfo
+                {
+                    Name = "reportsjs",
+                    EmbeddedResourcePath = GetType().Namespace + ".Web.reports.js"
+                }
+            };
+        }
+    }
+}
